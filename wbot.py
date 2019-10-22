e53b5ca5e8eb26f25d4cdacb26ae020d2d5a596f
Anselmo Junior <anselmo.junior@globalhitss.com.br>
2019-10-22 15:02:01 -0300

    teste

--- wbot.py
+++ wbot.py
@@ -8,74 +8,76 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# authenticator = IAMAuthenticator('njyivfAVLyznXwryzdRrNBIH5tO5rGaVO4W_TNxFnapg')
-authenticator = IAMAuthenticator(os.getenv('API_KEY'))
+if __name__ == '__main__':
 
-service = AssistantV1(
-    version='2019-02-08',
-    authenticator=authenticator
-)
+    # authenticator = IAMAuthenticator('njyivfAVLyznXwryzdRrNBIH5tO5rGaVO4W_TNxFnapg')
+    authenticator = IAMAuthenticator(os.getenv('API_KEY'))
 
-service.set_service_url('https://gateway.watsonplatform.net/assistant/api')
+    service = AssistantV1(
+        version='2019-02-08',
+        authenticator=authenticator
+    )
 
-# telegram = telepot.Bot("896415169:AAF-m60qjYm2WYFh36lpS8EWpiIbgn5CZ84")
-telegram = telepot.Bot(os.getenv('TELEGRAM_TOKEN'))
+    service.set_service_url('https://gateway.watsonplatform.net/assistant/api')
 
-context = {}
+    # telegram = telepot.Bot("896415169:AAF-m60qjYm2WYFh36lpS8EWpiIbgn5CZ84")
+    telegram = telepot.Bot(os.getenv('TELEGRAM_TOKEN'))
 
-
-def recebendoMsg(msg):
-
-    frase = msg['text']
-
-    if context is not None:
-
-        response = service.message(
-            workspace_id=os.getenv('WORKSPACE_ID'),
-            input={
-                'text': frase
-            },
-            context=context
-        ).get_result()
-
-        resp = response.get('output').get('text')
-
-        i = 0
-        for r in resp:
-            i += 1
-            tipoMsg, tipoChat, chatID = telepot.glance(msg)
-            if tipoMsg == 'text':
-                telegram.sendMessage(chatID, r)
-
-    else:
-        response = service.message(
-            workspace_id=os.getenv('WORKSPACE_ID'),
-            input={
-                'text': frase
-            }
-        ).get_result()
-
-        resp = response.get('output').get('text')
-
-        i = 0
-        for r in resp:
-            i += 1
-            tipoMsg, tipoChat, chatID = telepot.glance(msg)
-            if tipoMsg == 'text':
-                telegram.sendMessage(chatID, r)
-
-    def change_context():
-        global context
-        context = response.get('context')
-
-    change_context()
-
-    # tipoMsg, tipoChat, chatID = telepot.glance(msg)
-    # if tipoMsg == 'text':
-    #     telegram.sendMessage(chatID, resposta)
+    context = {}
 
 
-MessageLoop(telegram, recebendoMsg).run_as_thread()
+    def recebendoMsg(msg):
 
-while True:
-    pass
+        frase = msg['text']
+
+        if context is not None:
+
+            response = service.message(
+                workspace_id=os.getenv('WORKSPACE_ID'),
+                input={
+                    'text': frase
+                },
+                context=context
+            ).get_result()
+
+            resp = response.get('output').get('text')
+
+            i = 0
+            for r in resp:
+                i += 1
+                tipoMsg, tipoChat, chatID = telepot.glance(msg)
+                if tipoMsg == 'text':
+                    telegram.sendMessage(chatID, r)
+
+        else:
+            response = service.message(
+                workspace_id=os.getenv('WORKSPACE_ID'),
+                input={
+                    'text': frase
+                }
+            ).get_result()
+
+            resp = response.get('output').get('text')
+
+            i = 0
+            for r in resp:
+                i += 1
+                tipoMsg, tipoChat, chatID = telepot.glance(msg)
+                if tipoMsg == 'text':
+                    telegram.sendMessage(chatID, r)
+
+        def change_context():
+            global context
+            context = response.get('context')
+
+        change_context()
+
+        # tipoMsg, tipoChat, chatID = telepot.glance(msg)
+        # if tipoMsg == 'text':
+        #     telegram.sendMessage(chatID, resposta)
+
+
+    MessageLoop(telegram, recebendoMsg).run_as_thread()
+
+    while True:
+        pass
